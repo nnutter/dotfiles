787d0b4cec1df2aca52c96ccbafafeade32e8e28
Nathaniel Nutter <iam@nnutter.com>
2014-01-15 18:16:46 -0600

    support multi-machine commands

--- .bashrc.d/vagrant.sh
+++ .bashrc.d/vagrant.sh
@@ -1,16 +1,16 @@
 up_with_sandbox() {
-    command vagrant up --no-provision
-    command vagrant sandbox on
-    command vagrant provision
+    command vagrant up --no-provision "$@"
+    command vagrant sandbox on "$@"
+    command vagrant provision "$@"
 }
 
 vagrant_sup() {
     case $SB_MODE in
         on)
-            command vagrant up
+            command vagrant up "$@"
             ;;
         *)
-            up_with_sandbox
+            up_with_sandbox "$@"
             ;;
     esac
 }
@@ -18,12 +18,12 @@ vagrant_sup() {
 vagrant_rebuild() {
     case $SB_MODE in
         on)
-            command vagrant sandbox rollback
-            command vagrant provision
+            command vagrant sandbox rollback "$@"
+            command vagrant provision "$@"
             ;;
         *)
-            command vagrant destroy
-            up_with_sandbox
+            command vagrant destroy "$@"
+            up_with_sandbox "$@"
             ;;
     esac
 }
@@ -31,25 +31,25 @@ vagrant_rebuild() {
 vagrant() {
     SB_MODE=$(command vagrant sandbox status | sed 's/.*snapshot mode is //')
     ACTION=$1
+    shift
     case $ACTION in
         s|sup)
-            vagrant_sup
+            vagrant_sup "$@"
             ;;
         r|rebuild)
-            vagrant_rebuild
+            vagrant_rebuild "$@"
             ;;
-        help|-h|--help|'')
-            command vagrant help
+        -h|--help|'')
+            command vagrant help "$@"
             echo -e "\nAdditional Subcommands:"
             echo -e "\tsup - bring vagrant up but ensure sandbox is enabled before provisioning"
             echo -e "\trebuild - rollback (or destroy) and re-provision"
             ;;
         p)
-            shift
             command vagrant provision "$@"
             ;;
         *)
-            command vagrant "$@"
+            command vagrant $ACTION "$@"
             ;;
     esac
 }

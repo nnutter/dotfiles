a7335230540def83d5c4a723d88c27afda39a372
Nathaniel Nutter <iam@nnutter.com>
2014-02-26 17:34:34 -0600

    set extglob

--- .bashrc.d/shopt.sh
+++ .bashrc.d/shopt.sh
@@ -4,6 +4,7 @@ if [ "${BASH_VERSINFO[0]}" -ge "4" ]; then
 fi
 if [ "${BASH_VERSINFO[0]}" -ge "3" ]; then
     shopt -s cmdhist
+    shopt -s extglob
     shopt -s histappend
     shopt -s histverify
 fi

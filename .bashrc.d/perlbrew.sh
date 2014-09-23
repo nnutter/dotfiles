9d5553d508d59d2e2cc8a86fb6c3e25ad184bd84
Nathaniel Nutter <iam@nnutter.com>
2014-09-23 15:46:20 -0500

    perlbrew.sh updates

--- .bashrc.d/perlbrew.sh
+++ .bashrc.d/perlbrew.sh
@@ -1,3 +1,6 @@
+[[ -d "/usr/local/share/perlbrew" ]] && export PERLBREW_ROOT="/usr/local/share/perlbrew"
 [[ -d "$HOME/.perlbrew" ]] && export PERLBREW_ROOT="$HOME/.perlbrew"
+
+[[ -s "$PERLBREW_ROOT/etc/perlbrew-completion.bash" ]] && source "$PERLBREW_ROOT/etc/perlbrew-completion.bash"
 [[ -s "$PERLBREW_ROOT/etc/bashrc" ]] && source "$PERLBREW_ROOT/etc/bashrc"
 alias cpanm='cpanm --prompt'

0775232df58b1ceaf946e7a9024802b7b4f0d01a
Nathaniel Nutter <iam@nnutter.com>
2014-12-28 11:31:07 -0500

    add GOPATH/bin to PATH

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -2,4 +2,4 @@ GOCELLAR='/usr/local/opt/go/libexec/bin'
 test -d "$GOCELLAR" && export PATH=$PATH:$GOCELLAR
 
 GOPATH="${HOME}/.go"
-test -d "$GOPATH" && export GOPATH
+test -d "$GOPATH" && export GOPATH && export PATH=$PATH:$GOPATH/bin

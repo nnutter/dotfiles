7e5922d76312f151b296bb95fc2ae097b04b14fa
Nathaniel Nutter <iam@nnutter.com>
2015-01-28 02:16:01 -0600

    add $POSTGRES_HOME/bin to PATH

--- .bashrc.d/pg.sh
+++ .bashrc.d/pg.sh
@@ -3,6 +3,7 @@ POSTGRES_HOME=/Applications/Postgres.app/Contents/Versions/9.3
 if [ -d "$POSTGRES_HOME" ]
 then
     export POSTGRES_HOME
+    export PATH="${PATH}:${POSTGRES_HOME}/bin"
 else
     unset POSTGRES_HOME
 fi

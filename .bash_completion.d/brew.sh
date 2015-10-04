03f7cfd4aa930209c2c3c672d15dba646bcb49af
Nathaniel Nutter <iam@nnutter.com>
2015-10-04 11:58:29 -0500

    check if `brew` is known before calling
    On Fedora it tries to lookup which package provies `brew` if the command
    is not found which can be slow, e.g. if there are package locks.

--- .bash_completion.d/brew.sh
+++ .bash_completion.d/brew.sh
@@ -1,4 +1,6 @@
-BREW_PREFIX=$(brew --prefix 2> /dev/null)
-if [ -n "$BREW_PREFIX" ]; then
-    use "$BREW_PREFIX/etc/bash_completion"
+if type -p brew > /dev/null; then
+    BREW_PREFIX=$(brew --prefix 2> /dev/null)
+    if [ -n "$BREW_PREFIX" ]; then
+        use "$BREW_PREFIX/etc/bash_completion"
+    fi
 fi

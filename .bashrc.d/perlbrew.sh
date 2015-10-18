ed33b87d53ba8f230b43a41fb97a75af14bb4703
Nathaniel Nutter <iam@nnutter.com>
2015-10-18 12:53:47 -0500

    use test instead of [[ ]]

--- .bashrc.d/perlbrew.sh
+++ .bashrc.d/perlbrew.sh
@@ -1,7 +1,7 @@
-[[ -d "/usr/local/share/perlbrew/bin" ]] && export PERLBREW_ROOT="/usr/local/share/perlbrew"
-[[ -d "$HOME/.perlbrew/bin" ]] && export PERLBREW_ROOT="$HOME/.perlbrew"
+test -d "/usr/local/share/perlbrew/bin" && export PERLBREW_ROOT="/usr/local/share/perlbrew"
+test -d "$HOME/.perlbrew/bin" && export PERLBREW_ROOT="$HOME/.perlbrew"
 
-[[ -s "$PERLBREW_ROOT/etc/perlbrew-completion.bash" ]] && source "$PERLBREW_ROOT/etc/perlbrew-completion.bash"
-[[ -s "$PERLBREW_ROOT/etc/bashrc" ]] && source "$PERLBREW_ROOT/etc/bashrc"
-[[ -d "$PERLBREW_ROOT/bin" ]] && export PATH="$PATH:$PERLBREW_ROOT/bin"
+test -s "$PERLBREW_ROOT/etc/perlbrew-completion.bash" && source "$PERLBREW_ROOT/etc/perlbrew-completion.bash"
+test -s "$PERLBREW_ROOT/etc/bashrc" && source "$PERLBREW_ROOT/etc/bashrc"
+test -d "$PERLBREW_ROOT/bin" && export PATH="$PATH:$PERLBREW_ROOT/bin"
 alias cpanm='cpanm --prompt'

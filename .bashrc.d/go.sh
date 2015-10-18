d30543d333afc0d40e45152739aae6f43c14e3a1
Nathaniel Nutter <iam@nnutter.com>
2015-10-18 12:52:34 -0500

    also detect local install of Go

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -1,5 +1,5 @@
-GOCELLAR='/usr/local/opt/go/libexec/bin'
-test -d "$GOCELLAR" && export PATH=$PATH:$GOCELLAR
+test -d '/usr/local/opt/go/libexec/bin' && export PATH="$PATH:/usr/local/opt/go/libexec/bin"
+test -d '/usr/local/go/bin' && export PATH="$PATH:/usr/local/go/bin"
 
 GOPATH="${HOME}/.go"
 test -d "$GOPATH" && export GOPATH && export PATH=$PATH:$GOPATH/bin

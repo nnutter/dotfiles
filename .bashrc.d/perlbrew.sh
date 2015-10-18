7aad5f3314b4a463dac5064b9cdc73ba75754ee8
Nathaniel Nutter <iam@nnutter.com>
2015-10-18 12:48:47 -0500

    perlbrew made ~/.perlbrew so change PERLBREW_ROOT detection

--- .bashrc.d/perlbrew.sh
+++ .bashrc.d/perlbrew.sh
@@ -1,5 +1,5 @@
-[[ -d "/usr/local/share/perlbrew" ]] && export PERLBREW_ROOT="/usr/local/share/perlbrew"
-[[ -d "$HOME/.perlbrew" ]] && export PERLBREW_ROOT="$HOME/.perlbrew"
+[[ -d "/usr/local/share/perlbrew/bin" ]] && export PERLBREW_ROOT="/usr/local/share/perlbrew"
+[[ -d "$HOME/.perlbrew/bin" ]] && export PERLBREW_ROOT="$HOME/.perlbrew"
 
 [[ -s "$PERLBREW_ROOT/etc/perlbrew-completion.bash" ]] && source "$PERLBREW_ROOT/etc/perlbrew-completion.bash"
 [[ -s "$PERLBREW_ROOT/etc/bashrc" ]] && source "$PERLBREW_ROOT/etc/bashrc"

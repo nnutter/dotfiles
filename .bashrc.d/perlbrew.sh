b15dd65ee06a944ce8aee6196cf4d3bdb067548f
Nathaniel Nutter <iam@nnutter.com>
2015-10-18 12:49:32 -0500

    add PERLBREW_ROOT/bin to PATH

--- .bashrc.d/perlbrew.sh
+++ .bashrc.d/perlbrew.sh
@@ -3,4 +3,5 @@
 
 [[ -s "$PERLBREW_ROOT/etc/perlbrew-completion.bash" ]] && source "$PERLBREW_ROOT/etc/perlbrew-completion.bash"
 [[ -s "$PERLBREW_ROOT/etc/bashrc" ]] && source "$PERLBREW_ROOT/etc/bashrc"
+[[ -d "$PERLBREW_ROOT/bin" ]] && export PATH="$PATH:$PERLBREW_ROOT/bin"
 alias cpanm='cpanm --prompt'

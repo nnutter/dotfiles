8821474fc3e9709ab873454a59f77f4403325163
Nathaniel Nutter <iam@nnutter.com>
2015-12-11 23:35:52 -0600

    prevent GOPATH from being set if it doesn't exist

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -1,5 +1,4 @@
 test -d '/usr/local/opt/go/libexec/bin' && export PATH="$PATH:/usr/local/opt/go/libexec/bin"
 test -d '/usr/local/go/bin' && export PATH="$PATH:/usr/local/go/bin"
 
-GOPATH="${HOME}/.go"
-test -d "$GOPATH" && export GOPATH && export PATH=$PATH:$GOPATH/bin
+test -d "${HOME}/.go" && export GOPATH="${HOME}/.go" && export PATH="$PATH:${HOME}/.go/bin"

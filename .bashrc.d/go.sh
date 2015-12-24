0298cd54fcdfe3d645ceaa99cb9541935f98cc43
Nathaniel Nutter <iam@nnutter.com>
2015-12-24 17:10:23 -0600

    try storing GOPATH in XDG_CONFIG_HOME

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -1,4 +1,4 @@
 test -d '/usr/local/opt/go/libexec/bin' && export PATH="$PATH:/usr/local/opt/go/libexec/bin"
 test -d '/usr/local/go/bin' && export PATH="$PATH:/usr/local/go/bin"
 
-test -d "${HOME}/.go" && export GOPATH="${HOME}/.go" && export PATH="$PATH:${HOME}/.go/bin"
+test -d "${HOME}/.cache/go" && export GOPATH="${HOME}/.cache/go" && export PATH="$PATH:${HOME}/.cache/go/bin"

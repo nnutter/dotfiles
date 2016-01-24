ddb1353761a079df5286484d2528f1f407e9fc94
Nathaniel Nutter <iam@nnutter.com>
2016-01-23 23:58:37 -0600

    just use HOME as GOPATH

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -1,4 +1,4 @@
 test -d '/usr/local/opt/go/libexec/bin' && export PATH="$PATH:/usr/local/opt/go/libexec/bin"
 test -d '/usr/local/go/bin' && export PATH="$PATH:/usr/local/go/bin"
 
-test -d "${HOME}/.cache/go" && export GOPATH="${HOME}/.cache/go" && export PATH="$PATH:${HOME}/.cache/go/bin"
+export GOPATH="$HOME"

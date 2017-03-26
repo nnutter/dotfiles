ba67272332218a987edf2a2e85ad70fd432deb71
Nathaniel Nutter <iam@nnutter.com>
2017-03-25 22:40:17 -0500

    add SSH hostname completion for Bash

--- .bashrc.d/ssh.sh
+++ .bashrc.d/ssh.sh
@@ -9,3 +9,14 @@ function ssh-reagent {
     done
     echo Cannot find ssh agent - maybe you should reconnect and forward it?
 }
+function _ssh {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=$(grep '^Host' ~/.ssh/config | grep -v '[?*]' | cut -d ' ' -f 2-)
+
+    COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
+    return 0
+}
+complete -A hostname -F _ssh ssh

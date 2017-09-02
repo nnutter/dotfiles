be02a9cea266750c9f9af67ff32cfa95f85a9291
Nathaniel Nutter <iam@nnutter.com>
2017-09-02 15:19:11 -0500

    use a loop over directories

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -1,11 +1,11 @@
-if test -d '/usr/local/opt/go/libexec/bin'
-then
-    export PATH="${PATH}${PATH:+:}/usr/local/opt/go/libexec/bin"
-fi
-if test -d '/usr/local/go/bin'
-then
-    export PATH="${PATH}${PATH:+:}/usr/local/go/bin"
-fi
+GO_BIN_DIRS=('/usr/local/opt/go/libexec/bin' '/usr/local/go/bin')
+for DIR in "${GO_BIN_DIRS[@]}"
+do
+    if test -d "$DIR"
+    then
+        export PATH="${PATH}${PATH:+:}${DIR}"
+    fi
+done
 
 export GOPATH="${HOME}/go:${HOME}"
 export PATH="${HOME}/go/bin${PATH:+:}${PATH}"

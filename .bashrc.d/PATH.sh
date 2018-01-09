428798232169254593fda19b89998949787c4186
Nathaniel Nutter <iam@nnutter.com>
2018-01-08 22:13:17 -0600

    these are standard, no?

--- .bashrc.d/PATH.sh
+++ .bashrc.d/PATH.sh
@@ -18,16 +18,6 @@ then
     export PATH=${HOME}/bin${PATH:+:}${PATH}
 fi
 
-if test -d /usr/local/bin
-then
-    export PATH=/usr/local/bin${PATH:+:}${PATH}
-fi
-
-if test -d "/usr/local/sbin"
-then
-    PATH="$PATH${PATH:+:}/usr/local/sbin"
-fi
-
 if test -d "$HOME/.bin"
 then
     PATH="$PATH${PATH:+:}$HOME/.bin"

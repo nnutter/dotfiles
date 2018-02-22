374f183d2c3a5820c26e13b5009522f07cdfe311
Nathaniel Nutter <iam@nnutter.com>
2018-02-22 10:49:56 -0600

    enable hostname complete for `ssh` (Fedora didn't have built-in)

--- .bashrc.d/hosts.sh
+++ .bashrc.d/hosts.sh
@@ -1,2 +1,6 @@
 HOSTFILE=$HOME/.hosts
 complete -A hostname -o default curl dig host netcat ping telnet
+
+complete -r ssh 1>/dev/null || true
+complete -r mosh 1>/dev/null || true
+complete -A hostname -o default ssh mosh

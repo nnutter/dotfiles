4594d38da3a4b8270c41e2629c3cae2673e4a212
Nathaniel Nutter <iam@nnutter.com>
2018-07-24 11:59:18 -0500

    do not initialize pyenv-virtualenv if it's not installed

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -4,7 +4,10 @@ then
     export PYENV_ROOT
     export PATH="$PYENV_ROOT/bin${PATH:+:}$PATH"
     eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
+    if test -d "$PYENV_ROOT"/plugins/virtualenv/
+    then
+        eval "$(pyenv virtualenv-init -)"
+    fi
 else
     unset PYENV_ROOT
 fi

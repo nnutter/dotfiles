8344ca843fc98e31838b94537c54634a4a8b8cc5
Nathaniel Nutter <iam@nnutter.com>
2018-07-25 10:19:38 -0500

    add Homebrew's `bash_completion.d` directory

--- .bash_completion.d/brew.sh
+++ .bash_completion.d/brew.sh
@@ -2,5 +2,6 @@ if type -p brew > /dev/null; then
     BREW_PREFIX=$(brew --prefix 2> /dev/null)
     if [ -n "$BREW_PREFIX" ]; then
         use "$BREW_PREFIX/etc/bash_completion"
+        use "$BREW_PREFIX/etc/bash_completion.d"
     fi
 fi

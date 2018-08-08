982044dac048a9ce84ee146c60cf68bb310147b0
Nathaniel Nutter <iam@nnutter.com>
2018-08-08 11:09:42 -0500

    remove MGI `kinit` and `ssh` wrapper

--- .bashrc.d/ssh.sh
+++ .bashrc.d/ssh.sh
@@ -2,14 +2,6 @@ if test -z "$SSH_AUTH_SOCK"; then
     eval "$(ssh-agent -s)" 1>/dev/null
 fi
 
-function ssh {
-    if /usr/bin/which kinit 1>/dev/null 2>/dev/null
-    then
-        kinit
-    fi
-    command ssh "$@"
-}
-
 function ssh-reagent {
     for agent in /tmp/ssh-*/agent.*; do
         export SSH_AUTH_SOCK=$agent

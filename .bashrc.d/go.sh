0378cf21a293ab183f6e5df33e1cb8b1ace127b8
Nathaniel Nutter <iam@nnutter.com>
2019-04-18 21:04:48 -0500

    update recompile-go-tools

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -17,18 +17,20 @@ recompile-go-tools() {
         github.com/cweill/gotests/...
         github.com/davidrjenni/reftools/cmd/fillstruct
         github.com/fatih/gomodifytags
-        github.com/haya14busa/goplay/cmd/goplay
+        #github.com/haya14busa/goplay/cmd/goplay
         github.com/josharian/impl
-        github.com/mdempsky/gocode
+        #github.com/mdempsky/gocode
         github.com/ramya-rao-a/go-outline
         github.com/rogpeppe/godef
         github.com/uudashr/gopkgs/cmd/gopkgs
         github.com/zmb3/gogetdoc
         golang.org/x/lint/golint
-        golang.org/x/tools/cmd/godoc
+        #golang.org/x/tools/cmd/godoc
         golang.org/x/tools/cmd/goimports
         golang.org/x/tools/cmd/gorename
         golang.org/x/tools/cmd/guru
+        honnef.co/go/tools/cmd/staticcheck
+        honnef.co/go/tools/cmd/keyify
     )
     for TOOL in "${TOOLS[@]}"
     do

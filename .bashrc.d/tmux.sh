9f65f6debe1b305252431cacd6b337cd08123a63
Nathaniel Nutter <iam@nnutter.com>
2019-10-18 17:06:57 -0500

    Disable bash completion if not sourcing in bash

--- .bashrc.d/tmux.sh
+++ .bashrc.d/tmux.sh
@@ -1,13 +1,16 @@
-_tmuxs_complete() {
-    COMPREPLY=()
-    local SESSIONS=$(tmux list-sessions | cut -f 1 -d :)
-    if test ${#COMP_WORDS[@]} -eq 2
-    then
-        local cur=${COMP_WORDS[COMP_CWORD]}
-        COMPREPLY=($(compgen -W "$SESSIONS" -- $cur))
-    fi
-}
-complete -F _tmuxs_complete tmuxs
+if test -n "$BASH_VERSION"
+then
+    _tmuxs_complete() {
+        COMPREPLY=()
+        local SESSIONS=$(tmux list-sessions | cut -f 1 -d :)
+        if test ${#COMP_WORDS[@]} -eq 2
+        then
+            local cur=${COMP_WORDS[COMP_CWORD]}
+            COMPREPLY=($(compgen -W "$SESSIONS" -- $cur))
+        fi
+    }
+    complete -F _tmuxs_complete tmuxs
+fi
 
 function tmuxs {
     session_name=$1

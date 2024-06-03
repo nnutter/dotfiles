5845413161e740062a402009793f23872a8cd03a
Nathaniel Nutter <iam@nnutter.com>
2024-06-03 14:18:17 -0500

    Fix git-prune-branches

--- .bash_completion.d/brew.sh
+++
@@ -1,7 +0,0 @@
-if type -p brew > /dev/null; then
-    BREW_PREFIX=$(brew --prefix 2> /dev/null)
-    if [ -n "$BREW_PREFIX" ]; then
-        use "$BREW_PREFIX/etc/bash_completion"
-        use "$BREW_PREFIX/etc/bash_completion.d"
-    fi
-fi

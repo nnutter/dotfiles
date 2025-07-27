3358211c5d4fb69181314f71d0dfaf56e95a74b4
Nathan Nutter <nnutter@duck.com>
2025-07-26 20:04:07 -0500

    Add listchars

--- .config/nvim/lua/nnutter/set.lua
+++ .config/nvim/lua/nnutter/set.lua
@@ -3,6 +3,13 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 
+vim.opt.listchars = {
+	tab = "▸·",
+	trail = "·",
+	extends = "▸",
+}
+vim.opt.list = true
+
 vim.opt.number = true
 
 vim.opt.scrolloff = 8

bd47f92363c873200b46e7b42be2d96e73e48340
Nathan Nutter <nnutter@duck.com>
2025-07-26 19:31:03 -0500

    Make a Command + P quick open that works in or out of Git

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -5,7 +5,15 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope Git files' })
+vim.keymap.set('n', '<D-p>', function()
+  local is_git = os.execute('git rev-parse --is-inside-work-tree 2>/dev/null') == 0
+  if is_git then
+    builtin.git_files()
+  else
+    builtin.find_files()
+  end
+end, { desc = 'Telescope Git files' })
+
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, { desc = 'Telescope file search' })

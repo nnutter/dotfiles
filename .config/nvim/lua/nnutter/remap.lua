24401c2220595014b0ba8a92cd4c1abad239c680
Nathan Nutter <nnutter@duck.com>
2025-08-01 18:10:37 -0500

    Make Command + s save file in normal and insert

--- .config/nvim/lua/nnutter/remap.lua
+++ .config/nvim/lua/nnutter/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("i", "<D-s>", function() vim.api.nvim_command('write') end)
+vim.keymap.set("n", "<D-s>", function() vim.api.nvim_command('write') end)

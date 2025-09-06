d09f49c8e32975bbd6c419ea6a365ab27c74078a
Nathan Nutter <nnutter@duck.com>
2025-09-06 12:20:28 -0500

    Use Lazy's way of keymaps

--- .config/nvim/lua/plugins/refactoring.lua
+++ .config/nvim/lua/plugins/refactoring.lua
@@ -6,4 +6,62 @@ return {
   },
   lazy = false,
   opts = {},
+  keys = {
+    {
+      "<leader>re",
+      function()
+        return require("refactoring").refactor("Extract Function")
+      end,
+      expr = true,
+      mode = { "n", "x" },
+    },
+    {
+      "<leader>rf",
+      function()
+        return require("refactoring").refactor("Extract Function To File")
+      end,
+      expr = true,
+      mode = { "n", "x" },
+    },
+    {
+      "<leader>rv",
+      function()
+        return require("refactoring").refactor("Extract Variable")
+      end,
+      expr = true,
+      mode = { "n", "x" },
+    },
+    {
+      "<leader>rI",
+      function()
+        return require("refactoring").refactor("Inline Function")
+      end,
+      expr = true,
+      mode = { "n", "x" },
+    },
+    {
+      "<leader>ri",
+      function()
+        return require("refactoring").refactor("Inline Variable")
+      end,
+      expr = true,
+      mode = { "n", "x" },
+    },
+    {
+      "<leader>rbb",
+      function()
+        return require("refactoring").refactor("Extract Block")
+      end,
+      expr = true,
+      mode = { "n", "x" },
+    },
+    {
+      "<leader>rbf",
+      function()
+        return require("refactoring").refactor("Extract Block To File")
+      end,
+      expr = true,
+      mode = { "n", "x" },
+    },
+  },
 }

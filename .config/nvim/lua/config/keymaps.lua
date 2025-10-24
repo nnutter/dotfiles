afa265595cdb5bc31f7db27e05855aaa0d16f1bc
Nathan Nutter <nnutter@duck.com>
2025-10-24 12:03:34 -0500

    Reselect visual block after indent

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- reselect visual block after indent/outdent
+-- http://vimbits.com/bits/20
+vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
+vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
